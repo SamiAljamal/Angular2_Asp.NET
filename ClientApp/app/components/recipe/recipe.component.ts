c863db6e2737e946f50a5fa1ecba8eca8387dcda
Sami Al-Jamal <sami.m.aljamal>
2016-09-29 16:48:15 -0700

    attempt to create form.

--- ClientApp/app/components/recipe/recipe.component.ts
+++ ClientApp/app/components/recipe/recipe.component.ts
@@ -1,17 +1,32 @@
 ﻿import { Component } from '@angular/core';
 import { Http } from '@angular/http';
 
+
 @Component({
     selector: 'recipe',
+    outputs: ['onSubmitForm'],
     template: require('./recipe.component.html')
 })
 export class RecipeComponent {
     public recipes: Recipe[];
-
+    public isBusy: boolean;
     constructor(http: Http) {
         http.get('/api/SampleData/GetRecipes').subscribe(result => {
             this.recipes = result.json();
         });
+    }
+
+    addRecipe() {
+        this.isBusy = true;
+        $http.post('/api/SampleData/recipes', this.newRecipe)
+            .then(function () {
+                //success
+            }, function () {
+                //failure
+            })
+            .finally(function () {
+                this.isBusy = false;
+            });
     }
 }
 
